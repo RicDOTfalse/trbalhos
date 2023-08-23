8474736ba6e10c96efbbabe3f0d26655442c3b30
RicDOTfalse <143028960+RicDOTfalse@users.noreply.github.com>
2023-08-23 16:09:28 -0300

    Add files via upload

---
+++ trabalho - 230823.py
@@ -0,0 +1,16 @@
+A = float(input("insira A"  ))
+B = float(input("insira B"))
+C = float(input("insira o valor de C"))
+
+Delta = (B ** 2) - 4 * A * C  
+
+float(Delta) 
+
+equacao1 = (-B + (Delta ** 0.5)) / (2 * A)
+equacao2 = (-B - (Delta ** 0.5)) / (2 * A)
+
+print(equacao1)
+print(equacao2)
+
+
+
