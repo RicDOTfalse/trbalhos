f80596d1b83d7a45c3e30826afd390c16726ba1b
RicDOTfalse <143028960+RicDOTfalse@users.noreply.github.com>
2023-09-01 10:27:19 -0300

    Add files via upload

---
+++ projeto 2.py
@@ -0,0 +1,19 @@
+A = float(input("insira o primeiro numero"))
+
+B = float(input("insira o segundo numero"))
+
+C = float(input("insira o terceiro numero"))
+
+produto_n1 = (A * (B/2))
+
+produto_n2 = (A * 3 + (C))
+
+produto_n3 = (C ** 3)
+
+print(produto_n1)
+
+print(produto_n2)
+
+print(produto_n3)
+
+
---
+++ projeto.py
@@ -0,0 +1,11 @@
+valor_da_hora = float(input("me de o valor ganho por hora no seu trabalho"))
+
+horas_trabalhadas = float(input("quantas horas vc trabalhou ?"))
+
+pagamento = (valor_da_hora * horas_trabalhadas)
+
+print("você trabalhou por uma quantidade total de ", horas_trabalhadas , "horas ?")
+
+print("o valor da sua hora, e nisso numa unica hora e ", valor_da_hora )
+
+print("logo pelos calculos o total seria", pagamento)
